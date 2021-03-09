d7c16c385170d7e8d432214058feec3add49a9df
Jani Banigev <jani.bangiev@gmail.com>
2021-03-09 23:38:22 +0100

    - Added a better fullscreen mode
    Signed-off-by: Jani Banigev <jani.bangiev@gmail.com>

--- app/src/main/java/com/example/randomdrinks/GameActivity.java
+++ app/src/main/java/com/example/randomdrinks/GameActivity.java
@@ -1,11 +1,42 @@
 package com.example.randomdrinks;
 
 import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 //A special activity modified for game functions
 public class GameActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        hideSystemUI();
+    }
+
+    // This snippet hides the system bars.
+    protected void hideSystemUI() {
+        // Set the IMMERSIVE flag.
+        // Set the content to appear under the system bars so that the content
+        // doesn't resize when the system bars hide and show.
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
+    }
+
+    // This snippet shows the system bars. It does this by removing all the flags
+// except for the ones that make the content appear under the system bars.
+    protected void showSystemUI() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+    }
 
     // Stoping and starting the background music on pause and resume
     @Override
@@ -18,6 +49,7 @@ public class GameActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         SoundHandler.playBackgroundMusic();
+        hideSystemUI();
     }
 
     //Creates a popup Activity with all the messages
